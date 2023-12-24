18decce43de76db0672e561d0cca14372ca0fcf0
ananth <ananthpai9@gmail.com>
2023-12-23 23:37:42 -0500

    Accordion Issue open

--- force-app/main/default/lwc/projectPageTabs/projectPageTabs.js
+++ force-app/main/default/lwc/projectPageTabs/projectPageTabs.js
@@ -1,20 +1,32 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track} from 'lwc';
 import getProjectPageTabs from '@salesforce/apex/ProjectPageController.getProjectPageTabs';
 
 
 export default class ProjectPageTabs extends LightningElement {
 
     tabData;
+    activeAccordionSection;
 
     @wire(getProjectPageTabs)
     projectPageTabs({ error, data }){
         if (data) {
             console.log(data)
+            
+            
             this.tabData =  JSON.parse(data);
+            this.activeAccordionSection  =  this.tabData[0].categories[0].categoryName;
+            
+
+            console.log('this.activeAccordionSection: ', this.activeAccordionSection);
+            
         }
         else{
             console.log(error)
         }
+    }
+
+    handleAccordionSection(event){
+        this.activeAccordionSection = event.detail.name;
     }
 
 }
