6fbf1ad315b7a0f584fc35fdf0451cd680a83f1e
ananth <ananthpai9@gmail.com>
2023-12-25 23:34:56 -0500

    Fixed all known issue. completed project page. started modal component

--- force-app/main/default/lwc/projectPageTabs/projectPageTabs.js
+++ force-app/main/default/lwc/projectPageTabs/projectPageTabs.js
@@ -1,22 +1,23 @@
 import { LightningElement, wire, track} from 'lwc';
 import getProjectPageTabs from '@salesforce/apex/ProjectPageController.getProjectPageTabs';
+import MyModal from 'c/myModal';
 
 
 export default class ProjectPageTabs extends LightningElement {
 
     tabData;
     activeAccordionSection;
+    content = 'The modal content';
+    header = 'The modal header';
+
 
     @wire(getProjectPageTabs)
     projectPageTabs({ error, data }){
         if (data) {
-            console.log(data)
-            
-            
+            console.log(data)    
             this.tabData =  JSON.parse(data);
+            //need to fix this
             this.activeAccordionSection  =  this.tabData[0].categories[0].categoryName;
-            
-
             console.log('this.activeAccordionSection: ', this.activeAccordionSection);
             
         }
@@ -27,6 +28,15 @@ export default class ProjectPageTabs extends LightningElement {
 
     handleAccordionSection(event){
         this.activeAccordionSection = event.detail.name;
+    }
+
+    async handleShowModal() {
+        this.result = await MyModal.open({
+            size: 'small',
+            description: 'MiscModal displays the message in a popup',
+            header: this.header,
+            content: this.content
+        });
     }
 
 }
